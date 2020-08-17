7991bcaf5eb75534b5ab0868c2461a83449c77f8
Kao <66681691+kaodaopopi@users.noreply.github.com>
2020-08-17 14:51:02 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace USB_TRICK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_DEVICECHANGE = 0x219;
+            const int DBT_DEVICEARRIVAL = 0x8000;
+            const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+
+            object ojb = new object();
+            try
+            {
+                // WM_DEVICECHANGE Message : 電腦硬體裝置改變時產生的訊息
+                if (m.Msg == WM_DEVICECHANGE)
+                {
+                    switch (m.WParam.ToInt32())
+                    {
+                        case WM_DEVICECHANGE:
+                            break;
+                        // DBT_DEVICEARRIVAL Event : 裝置插入並且可以使用時，產生的系統訊息
+                        case DBT_DEVICEARRIVAL:
+                        // DBT_DEVICEREMOVECOMPLETE Event : 裝置卸載或移除時產生的系統訊息
+                        case DBT_DEVICEREMOVECOMPLETE:
+                            DeviceChange();
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            base.WndProc(ref m);
+        }
+
+        private void DeviceChange()
+        {
+            foreach (DriveInfo di in DriveInfo.GetDrives())
+            {
+                if (di.DriveType == DriveType.Removable)
+                {
+
+                    DirectoryInfo TheFolder = new DirectoryInfo(di.Name);
+                    //遍歷資料夾
+                    foreach (DirectoryInfo NextFolder in TheFolder.GetDirectories())
+                    {
+ 
+                        DirectoryInfo diMyDir = new DirectoryInfo(NextFolder.FullName);
+                        diMyDir.Attributes = FileAttributes.Hidden;
+                    }
+
+                    //遍歷檔案
+                    foreach (FileInfo NextFile in TheFolder.GetFiles())
+                    {
+  
+                        DirectoryInfo diMyDir = new DirectoryInfo(NextFile.FullName);
+                        diMyDir.Attributes = FileAttributes.Hidden;
+                    }
+
+                }
+            }
+
+        }
+    }
+}
+
